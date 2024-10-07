73f5bd38d79d45ec66c222432d57941ce6f75096
amritsagoo91 <amritsagoo91@gmail.com>
2024-10-07 14:20:34 -0700

    2.5 Separate module step 10

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,5 @@
 import { useState } from 'react'
-const Part=({part})=><p>{part.name} {part.exercises}</p>
-
-const Header = ({name}) => {
-  return (<h1>{name}</h1>)
-}
-
-const Content = ({parts}) => {
-  return <>
-    {
-      parts.map((part)=><Part key={part.id} part={part}/>)
-    }
-  </>
-}
-
-const Total =({parts})=>{
-  return<h4>total of {parts.reduce((sum,part)=>sum+part.exercises,0)} exercises</h4>
-}
-
-
-const Course = ({ course }) => {
-  return <>
-    <Header name={course.name} />
-    <Content parts={course.parts} />
-    <Total parts={course.parts}/>
-  </>
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
